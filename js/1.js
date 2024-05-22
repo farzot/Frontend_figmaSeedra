b90b6b139ffff349fafd92357f8da2a094da5b2d
= <=>
2024-05-22 09:52:44 +0500

    First comment

---
+++ js/1.js
@@ -0,0 +1,23 @@
+// // tailwind.config.js
+// module.exports = {
+//   theme: {
+//     extend: {
+//       keyframes: {
+     
+//       },
+//       animation: {
+//         moveAndChangeColor: "moveAndChangeColor 5s infinite",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+// @keyframes pulse {
+//   0%, 100% {
+//     opacity: 1;
+//   }
+//   50% {
+//     opacity: .5;
+//   }
+// }
